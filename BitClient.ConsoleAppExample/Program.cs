4d82053d7826d1eae3f1ff0a96313a1aac95f4cf
swagfin <39550012+swagfin@users.noreply.github.com>
2021-03-19 13:29:05 +0300

    Added ConsoleApp to implement BitClient

--- BitClient.ConsoleAppExample/Program.cs
+++ BitClient.ConsoleAppExample/Program.cs
@@ -14,7 +14,7 @@ namespace BitClient.ConsoleAppExample
         {
             ClientSample sample = new ClientSample();
             sample.SetupBanlist("banlist.txt");
-            await sample.LoadTorrentAsync("9D4A9495BE35D97B13E60D143F37CC38378D8233.torrent", "D:/TorrentsDownload");
+            await sample.LoadTorrentAsync("example.torrent", "D:/TorrentsDownload");
         }
 
 
